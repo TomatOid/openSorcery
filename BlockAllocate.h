9d8d92604735934707053dd8272eae7b9fecc47e
Connor <connormc757@gmail.com>
2020-06-07 17:36:17 -0400

    Moved some code into headers, bug fixes

--- BlockAllocate.h
+++ BlockAllocate.h
@@ -1,10 +1,12 @@
-#pragma once
+#ifndef BLOCKALLOCATE_H_INCLUDED
+#define BLOCKALLOCATE_H_INCLUDED
 #include <stdint.h>
+#include <stdlib.h>
 
 // this structure contains all of the relevant state for
 // a simple block allocator, which can be used when many
 // items of the same size need to be allocated dynamically
-typedef struct BlockPage
+typedef struct
 {
     void* pool;
     void** free;
@@ -13,8 +15,46 @@ typedef struct BlockPage
     size_t numblk;
 } BlockPage;
 
-void* blkalloc(BlockPage* page);
+#include <string.h>
 
-int blkfree(BlockPage* page, void* blk);
+void* blkalloc(BlockPage* page)
+{
+    if (page->top < 0) { return NULL; }
+    void* blk = page->free[page->top];
+    page->top--;
 
-void makePage(BlockPage* res, size_t num_blocks, size_t block_size);
+    memset(blk, 0, page->blksize);
+    return blk;
+}
+
+int blkfree(BlockPage* page, void* blk)
+{
+    if (page->top >= (__ssize_t)page->numblk) { return 0; } // possible double free
+    // check if blk is within the page's memory pool and is alligned properly
+    if (blk < page->pool || blk >= page->pool + page->numblk * page->blksize) { return 0; } // blk was outside of the valid reigon
+    if (((intptr_t)blk - (intptr_t)page->pool) % page->blksize != 0) { return 0; } // blk was not alligned
+    
+    page->free[page->top++] = blk;
+    return 1;
+}
+
+void makePage(BlockPage* res, size_t num_blocks, size_t block_size)
+{
+    // rounding block_size up to be a multiple of size void* to avoid any potential allignment issues
+    block_size = ((block_size + sizeof(void*) - 1) / sizeof(void*)) * sizeof(void*);
+    // allocate all the memory
+    res->pool = calloc(num_blocks, block_size);
+    res->free = calloc(num_blocks, sizeof(void*));
+
+    // now we need to fill up the free stack 
+    intptr_t ptr = (intptr_t)res->pool;
+    for (size_t i = 0; i < num_blocks; i++)
+    {
+        res->free[num_blocks - i - 1] = (void*)ptr;
+        ptr += block_size;
+    }
+    res->top = num_blocks - 1;
+    res->numblk = num_blocks;
+    res->blksize = block_size;
+} 
+#endif
--- HashTable.h
+++ HashTable.h
@@ -3,22 +3,66 @@
 #include <stdbool.h>
 #include "BlockAllocate.h"
 
-typedef struct hashItem
+typedef struct HashItem
 {
     uint64_t key;
     void* value;
-    struct hashItem* next;
-} hashItem;
+    struct HashItem* next;
+} HashItem;
 
-// A seprate chaining hash table implememtation
-typedef struct hashTable
+// A seprate chaining hash table struct
+typedef struct HashTable
 {
-    hashItem** items;
+    HashItem** items;
     BlockPage page;
     size_t len;
     size_t num;
-} hashTable;
+} HashTable;
 
-bool insertToTable(hashTable* table, uint64_t key, void* value);
-void* findInTable(hashTable* table, uint64_t key);
-void* removeFromTable(hashTable* table, uint64_t key);
+bool insertToTable(HashTable* table, uint64_t key, void* value)
+{
+    size_t hash = key % table->len;
+    // using a block allocator for this instead of
+    // using calloc to avoid heap fragmentation
+    HashItem* curr;
+    if (!(curr = blkalloc(&table->page))) return false;
+    curr->key = key;
+    curr->value = value;
+    // insert the item at the beginning of the linked list
+    curr->next = table->items[hash];
+    table->items[hash] = curr;
+    table->num++;
+    return true;
+}
+
+void* findInTable(HashTable* table, uint64_t key)
+{
+    size_t hash = key % table->len;
+    HashItem* curr = table->items[hash];
+
+    while (curr && curr->key != key) { curr = curr->next; }
+
+    if (curr) { return curr->value; }
+    else { return NULL; }
+}
+
+void* removeFromTable(HashTable* table, uint64_t key)
+{
+    size_t hash = key % table->len;
+    HashItem* curr = table->items[hash];
+    HashItem* prev = NULL;
+    // loop untill either we hit the end of the list or the keys match
+    while (curr && curr->key != key) { prev = curr; curr = curr->next; }
+
+    if (curr) 
+    { 
+        HashItem* after = (curr)->next;
+        void* value = (curr)->value;
+        if (prev) (prev)->next = after;
+        if (!blkfree(&table->page, curr)) exit(EXIT_FAILURE);
+        if (!prev) table->items[hash] = after;
+        table->num--;
+        return value;
+    }
+    else return NULL;
+}
